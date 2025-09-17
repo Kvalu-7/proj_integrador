c2f8a843775af8c265fedbd25f057400083cea5e
Kvalu-7 <cassiavalu7@gmail.com>
2025-09-17 18:15:01 -0300

    Update adm_aparencer_professor.py

--- cadastros_adm/adm_aparencer_professor.py
+++ cadastros_adm/adm_aparencer_professor.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
-from tkinter import messagebox
+from tkinter import messagebox              #==========OK===========
 
 class TelaCadastroProfessor(ttk.Frame):
     """
